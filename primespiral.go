d4b169e64e2f117ee28d433f6794b5c5953195ca
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2020-08-24 09:04:06 +0200

    Updated primespiral.go.

--- primespiral.go
+++ primespiral.go
@@ -1,36 +1,44 @@
 package main
 
 import (
+	"github.com/fogleman/gg"
 	"github.com/jbarham/primegen"
-	"image"
-	"image/color"
-	"image/draw"
 	"image/png"
 	"io"
+	"log"
 	"math"
-	"os"
+	"net/http"
 )
 
 func main() {
-	primespiral(os.Stdout)
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	primespiral(w)
 }
 
 func primespiral(out io.Writer) {
 	const (
 		size = 1000
 	)
-	rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-	img := image.NewRGBA(rect)
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 255, 255, 255}}, image.ZP, draw.Src)
+	dc := gg.NewContext(2 * size + 1, 2 * size + 1)
+	dc.DrawRectangle(0, 0, 2 * size + 1, 2 * size + 1)
+	dc.SetRGB(1, 1, 1)
+	dc.Fill()
 	g := primegen.New()
 	p := g.Next()
 	for p < 100000 {
 			r := float64(p)
 		    theta := float64(p)
-			x := int(size + r * math.Sin(theta) / 100.0 + 0.5)
-			y := int(size + r  * math.Cos(theta) / 100.0 + 0.5)
-			img.Set(x, y, color.RGBA{0, 0, 0, 255})
+			x := size + r * math.Sin(theta) / 100.0
+			y := size + r  * math.Cos(theta) / 100.0
+			dc.DrawCircle(x, y, 3)
+			dc.SetRGB(0, 0, 0)
+			dc.Fill()
 			p = g.Next()
 	}
+	img := dc.Image()
 	png.Encode(out, img)
 }
